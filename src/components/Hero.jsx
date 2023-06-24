c0e58ae59e9291118b2bc58e3575f87c6bab1a38
MaxiCacciamano <maximilianocacciamanomail.com>
2023-06-24 02:31:50 -0300

    001

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div>
+      <div>
+        {/* Overlay */}
+        <div>
+          <h1>the <span>Best</span></h1>
+          <h1>Foods <span>Delivered</span></h1>
+        </div>
+        <img src="https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&w=600" alt="/"/>
+      </div>
+    </div>
+  )
+}
