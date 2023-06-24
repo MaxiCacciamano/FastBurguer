f01197a014d36156f4d11c3b9dbad3e23f1bb2bd
MaxiCacciamano <maximilianocacciamanomail.com>
2023-06-24 03:04:18 -0300

    002

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import { NavBar } from './components/NavBar';
 import { Hero } from './components/Hero';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-7xl m-auto'>
       <NavBar/>
       <Hero/>
     </div>
